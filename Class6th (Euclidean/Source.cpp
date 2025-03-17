b5ad1c026caf756f6a374c21dea498b9dec45c82
jiseok08 <heoji0912@gmail.com>
2025-03-17 19:43:37 +0900

    (2025.3.17) 거품 정렬

---
+++ Class6th (Euclidean/Source.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int euclid(int x, int y)
+{
+	// 1. 두 개의 자연수 x, y가 있습니다.
+	//	  그리고 x와 y로 나눈 나머지를 R이라고 합니다.
+
+	// 2. R이 0이라면, x와 y의 최대공약수 y가 됩니다.
+
+	// 3. R이 0이 아니라면, x와 y의 최대 공약수는 y와 R의 
+	//    최대 공약수와 같기 때문에 x에는 y 그리고 y에는 R을
+	//    대입한 후 1번으로 돌아갑니다. 
+
+	if (y == 0)
+	{
+		return x;
+	}
+	else
+	{
+		return euclid(y, x % y);
+	}
+
+
+	cout << "최대 공약수 : " << y << endl;
+}
+
+int main()
+{
+#pragma region 유클리드 호제버
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+	int a = 106;
+	int b = 16;
+
+	cout << euclid(a, b) << endl;
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,45 +1,9 @@
 ﻿#include <iostream>
 
-#define SIZE 5
-
 using namespace std;
 
 int main()
 {
-#pragma region 삽입 정렬
-	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
-	// 자신의 위치를 찾아 삽입하는 방식으로 정렬되는 알고리즘입니다.
-
-	int list[] = { 9,6,8,1,3 };
-
-	for (int i = 0; i < (SIZE - 1); i++)
-	{
-		int key = list[i + 1];
-
-		if (list[i] > key)
-		{
-			list[i + 1] = list[i];
-
-			list[i] = key;
-		}
-		if (i >= 3)
-		{
-			for (int j = i; j > 0; j--)
-			{
-				key = list[j];
-				if (list[j - 1] > list[j])
-				{
-
-				}
-			}
-		}
-	}
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << list[i] << " ";
-	}
-#pragma endregion
 
 
 	return 0;
