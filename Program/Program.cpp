5f51dd6f29fb8bd0775e1a3c7ccb95cb7de1ecca
jiseok08 <heoji0912@gmail.com>
2025-03-25 19:44:55 +0900

    (2025.3.25) 병합 정렬

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,63 +2,60 @@
 
 using namespace std;
 
-#define SIZE 8
+#define SIZE 100001
 
-void merge_sort(int list[], int start, int end)
+int list[SIZE];
+
+int Fibonacci(int n)
 {
-    if (start < end)
+    if (n <= 0)
     {
-        int middle = (start + end) / 2;
-
-        merge_sort(list, start, middle);
-        merge_sort(list, middle, end);
+        return 0;
     }
-}
-
-void combine(int list[], int start, int middle, int end)
-{
-    int count = 0;
-    int left = start;
-    int right = middle + 1;
-
-    int* container = new int[end - start + 1];
-
-    // 두 두분의 배열을 병합합니다.
-    while (left <= middle && right <= end)
+    else if (n <= 2)
     {
-        if (list[left] <= list[right])
-        {
-            container[count++] = list[left++];
-        }
-        else
-        {
-            container[count++] = list[right++];
-        }
+        return 1;
     }
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (i <= 0)
+        {
+            list[i]= 0;
+        }
+        else if (i <= 2)
+        {
+            list[i] = 1;
+        }
+
+        list[i] = list[i - 1] + list[i - 2];
+    }
+        return list[n];
 }
 
 int main()
 {
-#pragma region 병합 정렬
-    // 하나의 리스트를 두 개의 균일한 크기로 분할하고 분할된
-    // 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를
-    // 합하여 전체가 정렬된 리스트가 되게 하는 방법입니다.
+#pragma region 동적 계획법
+    // 특정 범위까지의 값을 구하기 위해 그것과
+    // 다른 범위까지의 값을 이용해서 효율적으로 값을
+    // 구하는 알고리즘입니다.
 
-    int list[] = { 3,5,2,7,4,1,8,6 };
+    // (Overlapping Subproblems) 겹치는 부분 문제
+    // 동일한 작은 문제들이 반복하여 나타나는 경우를 의미합니다.
 
-    merge_sort(list, 0, SIZE-1);
+    // (Optimal Substructure) 최적 부분 구조
+    // 부분 문제의 최적 결과 값을 사용하여 전체 문제의 최적
+    // 결과를 낼 수 있는 경우를 의미합니다.
 
-    // 1. 리스트의 길이가 0 또는 1이면 이미 정렬된 것으로 봅니다.
+    // 메모이제이션 (Memoization)
+    // 프로그램이 동일한 계산을 반복해야 할 때, 이전에 계산한 값을
+    // 메모리에 저장함으로써 동일한 계산을 반복 수행하는 작업을
+    // 제거하여 프로그램의 실행 속도를 향상시키는 방법입니다.
 
-    // 2. 그렇지 않은 경우
-
-    // 2-1. 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의
-    // 두 부분 리스트로 나눕니다.
-
-    // 2-2. 각 부분 리스트를 재귀적으로 병합 정렬을 이용하여 정렬합니다.
-
-    // 2-3. 두 두분 리스트를 다시 하나의 정렬된 리스트로 병합합니다.
+    cout << Fibonacci(4) << endl;
+    cout << list[3] << endl;
 #pragma endregion
+
 
 
     return 0;
