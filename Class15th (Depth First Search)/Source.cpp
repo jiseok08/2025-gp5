995700108931db024748cfb726baf4aed648414f
jiseok08 <heoji0912@gmail.com>
2025-03-28 19:43:29 +0900

    (2025.3.28) 깊이 우선 탐색

---
+++ Class15th (Depth First Search)/Source.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <vector>
+
+#define SIZE 8
+
+using namespace std;
+
+class Node
+{
+private:
+    bool visited[SIZE];
+    vector<int> adjacencyList[SIZE];
+public:
+    Node()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            visited[i] = false;
+        }
+    }
+    void insert(int i, int j)
+    {
+        adjacencyList[i].push_back(j);
+        adjacencyList[j].push_back(i);
+    }
+    void Search(int start)
+    {
+        // 현재 노드를 방문한 것으로 표시합니다.
+        visited[start] = true;
+
+        // 현제 노드를 출력합니다.
+        cout << start << " ";
+
+        // 현재 노드와 연결된 다른 노드를 재귀적으로 방문합니다.
+        for (int i = 0; i < adjacencyList[start].size(); i++)
+        {
+            // 현제 노드와 연결된 다음 노드를 가져옵니다.
+            int next = adjacencyList[start][i];
+
+            if (visited[next] == false)
+            {
+                // 다음 노드가 방문하지 않은 노드라면 'Search' 함수를 호출합니다.
+                Search(next);
+            }
+        }
+    }
+};
+
+int main()
+{
+#pragma region 깊이 우선 탐색 (Depth First Search)
+    // 노드에서부터 다음 분기로 넘어가기 전에
+    // 해당 분기를 완벽하게 탐색하는 방법입니다.
+
+    // 깊이 우선 탐색은 자료구조 Stack을 활용합니다.
+
+    // 1. 시작 노드를 스택에 넣고 방문 처리를 합니다.
+
+    // 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가
+    //    있으면 그 노드를 스택에 넣고 방문 처리합니다.
+
+    // 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단에
+    //    있는 노드를 꺼냅니다.
+
+    // 4. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복합니다.
+
+    Node node;
+
+    node.insert(1, 2);
+    node.insert(1, 3);
+
+    node.insert(2, 3);
+    node.insert(2, 4);
+    node.insert(2, 5);
+
+    node.insert(3, 6);
+    node.insert(3, 7);
+
+    node.insert(4, 5);
+    node.insert(6, 7);
+
+    node.Search(1);
+
+#pragma endregion
+
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <queue>
 
 #define SIZE 8
 
@@ -8,6 +9,9 @@ using namespace std;
 class Node
 {
 private:
+
+    queue<int> queue;
+
     bool visited[SIZE];
     vector<int> adjacencyList[SIZE];
 public:
@@ -19,56 +23,59 @@ public:
         }
     }
     void insert(int i, int j)
-    { 
+    {
         adjacencyList[i].push_back(j);
         adjacencyList[j].push_back(i);
     }
-    void search(int start)
+    void Search(int start)
     {
-        if (adjacencyList[start] != NULL) // 인접한 리스트가 있다면 연결한다
+        queue.push(start);
+        visited[start] = true;
+        while (queue.empty() == false)
         {
+            int x = queue.front();
 
+            queue.pop();
+
+            cout << x << " ";
+
+            for (int i = 0; i < adjacencyList[x].size(); i++)
+            {
+                int next = adjacencyList[x][i];
+
+                if (visited[next] == false)
+                {
+                    queue.push(next);
+
+                    visited[next] = true;
+                }
+            }
         }
-
-        search();
     }
 };
 
 int main()
 {
-#pragma region 깊이 우선 탐색 (Depth First Search)
-    // 노드에서부터 다음 분기로 넘어가기 전에
-    // 해당 분기를 완벽하게 탐색하는 방법입니다.
-
-    // 깊이 우선 탐색은 자료구조 Stack을 활용합니다.
-
-    // 1. 시작 노드를 스택에 넣고 방문 처리를 합니다.
-
-    // 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가
-    //    있으면 그 노드를 스택에 넣고 방문 처리합니다.
-
-    // 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단에
-    //    있는 노드를 꺼냅니다.
-
-    // 4. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복합니다.
+#pragma region 너비 우선 탐색 (Breadth Fisrt Search)
+    // 시작 정점을 방문한 후 시작 정점에 인접한
+    // 모든 정점들을 우선 방문하는 방법입니다.
 
     Node node;
 
     node.insert(1, 2);
     node.insert(1, 3);
 
-    node.insert(2, 3);
     node.insert(2, 4);
     node.insert(2, 5);
 
     node.insert(3, 6);
     node.insert(3, 7);
 
-    node.insert(4, 5);
-    node.insert(6, 7);
+    node.Search(1);
 
+    // 더 이상 방문하지 않은 정점들이 없을 때까지 방문하지 않은
+    // 모든 정점들에 대해서도 너비 우선 탐색을 적용합니다.
 #pragma endregion
-
 
     return 0;
 }
