57ff3b9b95adcc6cac82c6c764adef3057240d12
jiseok08 <heoji0912@gmail.com>
2025-04-01 19:44:49 +0900

    (2025.4.1) 위상 정렬

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,89 +2,68 @@
 #include <queue>
 #include <vector>
 
-#define SIZE 8
+#define SIZE 6
+
+int parent[SIZE];
 
 using namespace std;
 
-class Graph
+// Root Node를 찾는 함수
+
+// 배열의 인덱스와 값이 같다면 Root Node 발견
+
+// 부모 노드의 번호를 전달하면서, Root Node를 찾을 때까지 
+// 재귀 호출을 반복합니다.
+int find(int x)
 {
-private:
-    int degree[SIZE];
-    queue<int> queue;
-    vector<int> adjacencyList[SIZE];
-public:
-    ~Graph()
+    if (parent[x] == x)
     {
-        for (int i = 0; i < SIZE; i++)
-        {
-            degree[i] = 0;
-        }
+        return x;
     }
-
-    void insert(int vertex, int edge)
+    else
     {
-        adjacencyList[vertex].push_back(edge);
-
-        degree[edge]++;
+        return parent[x] = find(parent[x]);
     }
+    
+}
+void Union(int x, int y)
+{
+    x = find(x);
+    y = find(y);
 
-    void sort()
+    if (x == y)  {return;}
+    if ()
+}
+bool same(int x, int y)
+{
+    if (x == y)
     {
-        for (int i = 1; i < SIZE; i++)
-        {
-
-            if (degree[i] == 0)
-            {
-                queue.push(i);
-            }
-
-            int x = queue.front();
-
-            queue.pop();
-
-            cout << x << " ";
-        }
+        return true;
     }
-};
+    else
+    {
+        return false;
+    }
+}
 
 int main()
 {
-#pragma region 위상 정렬
-    // 병합 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
-    // 모든 정점을 자례대로 진행하는 알고리즘입니다.
+#pragma region 유니온 파인드
+    // 여러 노드가 존재할 때 어떤 노드가 다른 노드와 
+    // 연결되어 있는지 확인하는 알고리즘입니다.
 
-    // 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
+    // Union : 특정한 두 개의 노드를 같은 집합으로 합치는 연산입니다.
 
-    // DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
-
-    // 시간 복잡도 :  O(V + E)
-
-    // 위상 정렬하는 방법
-
-    // 1. 진입 차수가 0인 정점을 Queue에 삽입합니다.
-
-    // 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
-
-    // 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
-
-    // 4. Queue가 비어있을 때까지 2번 ~ 3번 작업을 반복 수행합니다.
-
-    Graph graph;
-
-    graph.insert(1, 2);
-    graph.insert(1, 5);
-
-    graph.insert(2, 3);
-    graph.insert(3, 4);
-
-    graph.insert(4, 6);
-
-    graph.insert(5, 6);
-    graph.insert(6, 7);
-
-    graph.sort();
+    // Find : 특정한 노드가 어느 집합에 있는지 확인하는 연산입니다.
 #pragma endregion
+    for (int i = 0; i < SIZE; i++)
+    {
+        parent[i] = i;
+    }
+    parent[5] = 4;
+    parent[4] = 3;
 
+    cout << Find(5) << endl;
 
     return 0;
 }
