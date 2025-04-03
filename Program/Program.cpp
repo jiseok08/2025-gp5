c6ee3c6b66f6e874abf5247a11ff8d3d281a0ce7
jiseok08 <heoji0912@gmail.com>
2025-04-03 19:44:27 +0900

    (2025.4.3) 집합

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
+#include <algorithm>
 #include <vector>
 
-using namespace std;
-
 #define SIZE 8
+
+using namespace std;
 
 class Kruskal
 {
@@ -21,6 +22,15 @@ private:
             this->vertexY = vertexY;
             this->weight = weight;
         }
+
+        const int& VertexX() { return vertexX; };
+        const int& VertexY() { return vertexY; };
+        const int& Weight() { return weight; };
+        
+        const bool& operator < (const Edge& edge)
+        {
+            return weight < edge.weight;
+        }
     };
 
     int cost;
@@ -34,26 +44,68 @@ public:
 
         for (int i = 0; i < SIZE; i++)
         {
-            parent[i] = 0;
+            parent[i] = i;
         }
     }
+
     void insert(int vertexX, int vertexY, int weight)
     {
         Edge edge(vertexX, vertexY, weight);
 
         nodeList.push_back(edge);
     }
+
+    void calculate()
+    {
+        sort(nodeList.begin(), nodeList.end());
+
+        for (int i = 0; i < nodeList.size(); i++)
+        {
+            if (same(nodeList[i].VertexX(), nodeList[i].VertexY()) == false)
+            {
+                cost += nodeList[i].Weight();
+
+                Union(nodeList[i].VertexX(), nodeList[i].VertexY());
+            }
+        }
+
+        cout << "Cost : " << cost << endl;
+    }
+
+    int find(int x)
+    {
+        if (parent[x] == x)
+        {
+            return x;
+        }
+        else
+        {
+            return parent[x] = find(parent[x]);
+        }
+
+    }
+    void Union(int x, int y)
+    {
+        x = find(x);
+        y = find(y);
+
+        if (x < y) { parent[y] = x; }
+        else { parent[x] = y; }
+    }
+    bool same(int x, int y)
+    {
+        return find(x) == find(y);
+    }
+
 };
 
 int main()
 {
 #pragma region 최소 신장 트리
-    // 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+    // 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는 
     // 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
 
     // 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
-
-#pragma endregion
 
     Kruskal kruskal;
 
@@ -67,8 +119,16 @@ int main()
     kruskal.insert(1, 5, 19);
 
     kruskal.insert(5, 7, 73);
-    kruskal.insert(5, 6, 48);
+    kruskal.insert(2, 5, 61);
+
     kruskal.insert(5, 3, 22);
+    kruskal.insert(5, 6, 48);
+    kruskal.insert(3, 6, 36);
+
+    kruskal.calculate();
+
+
+#pragma endregion
 
 
     return 0;
